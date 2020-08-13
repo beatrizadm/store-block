3893548c1e73d5b8d7e1404248ec51d3c1910901
Beatriz Miranda <beatriza.dmiranda@gmail.com>
2020-08-13 01:37:46 -0300

    issue#8

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,32 +1,70 @@
-import React, { useState } from 'react'
-import { TimeSplit } from './typings/global'
-import { tick } from './utils/time'
-import { useCssHandles } from 'vtex.css-handles'
+import React, { useState } from 'react';
+import { TimeSplit } from './typings/global';
+import { tick } from './utils/time';
+import { useCssHandles } from 'vtex.css-handles';
+import { useQuery } from 'react-apollo';
+import useProduct from 'vtex.product-context/useProduct';
+import productReleaseDate from './queries/productReleaseDate.graphql';
+
+
+const DEFAULT_TARGET_DATE = (new Date('2020-08-15')).toISOString();
+const CSS_HANDLES = ['countdown'];
 
 interface CountdownProps {
   targetDate: string
 }
 
-const DEFAULT_TARGET_DATE = (new Date('2020-08-08')).toISOString()
-const CSS_HANDLES = ['countdown']
-
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
-  targetDate = DEFAULT_TARGET_DATE,
-}) => {
-  const [timeRemaining, setTime] = useState<TimeSplit>({
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({  }) => {
+  const [ timeRemaining, setTime ] = useState<TimeSplit> ({
     hours: '00',
     minutes: '00',
     seconds: '00'
-  })
+  });
+  
+  const handles = useCssHandles(CSS_HANDLES);
+  
+  //const { product: { linkText } } = useProduct();
+  const  productContext = useProduct()
+  const product = productContext?.product
+  const linkText = product?.linkText
+  
+  const { data, loading, error } = useQuery(productReleaseDate, {
+    variables: {
+      slug: linkText
+    },
+    ssr: false
+  });
 
-  const handles = useCssHandles(CSS_HANDLES)
+  if (loading) {
+    return (
+      <div>
+        <span>Loading...</span>
+      </div>
+    );
+  }
 
-  tick(targetDate, setTime)
+  if (error) {
+    return (
+      <div>
+        <span>Erro!</span>
+      </div>
+    );
+  }
+
+  if (!product) {
+    return (
+      <div>
+        <span>Não há contexto de produto</span>
+      </div>
+    );
+  }
+
+  tick(data?.product?.releaseDate || DEFAULT_TARGET_DATE, setTime);
 
   return (
-    <div className={`${handles.countdown} t-heading-2 fw3 w-100 c-muted-1 db tc`}>
-      {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
-    </div>
+      <div className={`${handles.countdown} t-heading-2 fw3 w-100 c-muted-1 db tc`}>
+        { `${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}` }
+      </div>
   )
 }
 
@@ -40,8 +78,8 @@ Countdown.schema = {
       description: 'Data final utilizada no contador',
       type: 'string',
       default: null,
-    },
+    }
   },
 }
 
-export default Countdown
+export default Countdown;
